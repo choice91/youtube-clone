12d527fe9ecd142ef1783e1c3a5743c56483b0e0
LST <wer1264@gmail.com>
2021-09-21 11:34:45 +0900

    검색, 수정기능
    findById로 검색, exists로 데이터 유무확인, findByIdAndUpdate로 수정

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -15,12 +15,28 @@ export const watch = async (req, res) => {
 };
 
 export const getEdit = async (req, res) => {
-  return res.render("edit", { pageTitle: `Editing`, video });
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video Not Found" });
+  }
+  return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video Not Found" });
+  }
+  await Video.findByIdAndUpdate(id, {
+    title: title,
+    description: description,
+    hashtags: hashtags
+      .split(",")
+      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+  });
   return res.redirect(`/videos/${id}`);
 };
 
@@ -34,7 +50,9 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title: title,
       description: description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags: hashtags
+        .split(",")
+        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
     });
     return res.redirect("/");
   } catch (error) {
