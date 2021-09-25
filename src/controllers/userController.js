cb983f5f7abd99c7f9efc73c88aabe445940729c
LST <wer1264@gmail.com>
2021-09-25 17:16:37 +0900

    profile 수정
    수정하고자 하는 username, email이 이미 DB에 있으면 수정하지 않고 에러메시지 출력. findByIdAndUpdate로 프로필 수정 및 세션정보 업데이트

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -154,8 +154,28 @@ export const getEdit = (req, res) => {
   return res.render("edit-profile", { pageTitle: "Edit Profile" });
 };
 
-export const postEdit = (req, res) => {
-  return res.render("edit-profile");
+export const postEdit = async (req, res) => {
+  const {
+    session: {
+      user: { _id },
+    },
+    body: { name, email, username, location },
+  } = req;
+  const findUsername = await User.findOne({ username });
+  const findEmail = await User.findOne({ email });
+  if (findUsername._id !== _id || findEmail._id !== _id) {
+    return res.render("edit-profile", {
+      pageTitle: "Edit Profile",
+      errorMessage: "User is exists",
+    });
+  }
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    { name, email, username, location },
+    { new: true }
+  );
+  req.session.user = updatedUser;
+  return res.redirect("/users/edit");
 };
 
 export const see = (req, res) => {
