af3644c2bf806558057a693d64e0b7bdc9721d0b
LST <wer1264@gmail.com>
2021-09-27 20:51:52 +0900

    프로필 이미지 업로드

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -157,21 +157,28 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id },
+      user: { _id, avatarUrl },
     },
     body: { name, email, username, location },
+    file,
   } = req;
-  const findUsername = await User.findOne({ username });
-  const findEmail = await User.findOne({ email });
-  if (findUsername._id !== _id || findEmail._id !== _id) {
-    return res.render("edit-profile", {
-      pageTitle: "Edit Profile",
-      errorMessage: "User is exists",
-    });
-  }
+  // const findUsername = await User.findOne({ username });
+  // const findEmail = await User.findOne({ email });
+  // if (findUsername._id !== _id || findEmail._id !== _id) {
+  //   return res.render("edit-profile", {
+  //     pageTitle: "Edit Profile",
+  //     errorMessage: "User is exists",
+  //   });
+  // }
   const updatedUser = await User.findByIdAndUpdate(
     _id,
-    { name, email, username, location },
+    {
+      avatarUrl: file ? file.path : avatarUrl,
+      name,
+      email,
+      username,
+      location,
+    },
     { new: true }
   );
   req.session.user = updatedUser;
