65baa8d9d409cfba29c474b5664c83a6aeddffaa
LST <wer1264@gmail.com>
2021-10-04 18:56:58 +0900

    comment 화면에 출력

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -11,7 +11,8 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
+  console.log(video);
   if (!video) {
     return res.render("404", { pageTitle: "Video Not Found" });
   }
@@ -144,5 +145,8 @@ export const createComment = async (req, res) => {
     owner: user._id,
     video: id,
   });
+  console.log(comment);
+  video.comments.push(comment._id);
+  video.save();
   return res.sendStatus(201);
 };
